e09492d12a9d7892277e34bb55dde14758c4fe6f
kryla-angela <krylaangel@gmail.com>
2025-06-24 23:03:14 +0300

    corected style mobaile header-menu

--- src/modules/home/components/CategoriesSection/CategoriesSection.tsx
+++ src/modules/home/components/CategoriesSection/CategoriesSection.tsx
@@ -1,15 +1,15 @@
 import Card from './Card'
 
 function CategoriesSection() {
-  return (
-    <section className="clamp">
-      <div className="grid grid-cols-3 gap-4 rounded-[10px] py-5 sm:py-12">
-        <Card imgURL="categoriesSection/ForHere.png" text="Для неї"></Card>
-        <Card imgURL="categoriesSection/ForHim.png" text="Для нього"></Card>
-        <Card imgURL="categoriesSection/Accessories.png" text="Аксесуари"></Card>
-      </div>
-    </section>
-  )
+    return (
+        <section className="clamp">
+            <div className="grid grid-cols-3 gap-x-[14px] rounded-[10px] py-5 sm:py-12">
+                <Card imgURL="categoriesSection/ForHere.png" text="Для неї"></Card>
+                <Card imgURL="categoriesSection/ForHim.png" text="Для нього"></Card>
+                <Card imgURL="categoriesSection/Accessories.png" text="Аксесуари"></Card>
+            </div>
+        </section>
+    )
 }
 
 export default CategoriesSection
